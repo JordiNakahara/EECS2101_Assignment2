50fc06b0c617e59fac25d150e3287325c4100568
Aadi Gautam <124839461+Aadigautam2003@users.noreply.github.com>
2023-11-11 16:53:46 -0500

    Update Round_Robin.java

--- src/Round_Robin.java
+++ src/Round_Robin.java
@@ -6,86 +6,93 @@ import java.util.Queue;
 import java.util.LinkedList;
 import java.util.Scanner;
 public class Round_Robin {
+    
+    public String scheduler(RobinProcess[] p, int tq, int at, int bt) {
+        int n = p.length;
+        int index;
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[100];
+        boolean is_first_process = true;
+        int current_time = 0, max_completion_time;
+        int completed = 0, total_idle_time = 0;
+        float sum_tat = 0, sum_wt = 0, sum_rt = 0;
+        for (int i = 0; i < n; i++) {
+            p[i] = new RobinProcess(i, at, 0);
+            p[i].pid=i;
+        }
+        for (int i = 0; i < n; i++) {
+            p[i].bt = bt;
+            p[i].bt_remaining = p[i].bt;
+        }
 
- public String scheduler(RobinProcess[] p, int tq) {
-            int n = p.length;
-            int index;
-            Queue<Integer> q = new LinkedList<>();
-            boolean[] visited = new boolean[100];
-            boolean is_first_process = true;
-            int current_time = 0, max_completion_time;
-            int completed = 0, total_idle_time = 0, length_cycle;
-            float sum_tat = 0, sum_wt = 0, sum_rt = 0;
-            for (int i = 0; i < n; i++) {
-                p[i] = new Process(i, at, 0);
+        //We have sorted the structure in the increasing order on the basis of arrival time
+        Arrays.sort(p, 0, n, new RobinProcess.ProcessComparatorAT());
+        q.add(0);
+        visited[0] = true;
+        while (completed != n) {
+            index = q.poll();
+            if (p[index].bt_remaining == p[index].bt) {
+                p[index].start_time = Math.max(current_time, p[index].at);
+                total_idle_time += (is_first_process == true) ? 0 : p[index].start_time - current_time;
+                current_time = p[index].start_time;
+                is_first_process = false;
             }
-            for (int i = 0; i < n; i++) {
-                p[i].bt = bt;
-                p[i].bt_remaining = bt;
+            if (p[index].bt_remaining - tq > 0) {
+                p[index].bt_remaining -= tq;
+                current_time += tq;
+            } else {
+                current_time += p[index].bt_remaining;
+                p[index].bt_remaining = 0;
+                completed++;
+                p[index].ct = current_time;
+                p[index].tat = p[index].ct - p[index].at;
+                p[index].wt = p[index].tat - p[index].bt;
+                p[index].rt = p[index].start_time - p[index].at;
+                sum_tat += p[index].tat;
+                sum_wt += p[index].wt;
+                sum_rt += p[index].rt;
             }
-            Arrays.sort(p, 0, n, new ProcessComparatorAT());
-            q.add(0);
-            visited[0] = true;
-            while (completed != n) {
-                index = q.poll();
-                if (p[index].bt_remaining == p[index].bt) {
-                    p[index].start_time = Math.max(current_time, p[index].at);
-                    total_idle_time += (is_first_process == true) ? 0 : p[index].start_time - current_time;
-                    current_time = p[index].start_time;
-                    is_first_process = false;
+    // we have to check which new processes needs to be pushed to Ready Queue from the Input list
+            for (int i = 1; i < n; i++) {
+                if (p[i].bt_remaining > 0 && p[i].at <= current_time && visited[i] == false) {
+                    q.add(i);
+                    visited[i] = true;
                 }
-                if (p[index].bt_remaining - tq > 0) {
-                    p[index].bt_remaining -= tq;
-                    current_time += tq;
-                } else {
-                    current_time += p[index].bt_remaining;
-                    p[index].bt_remaining = 0;
-                    completed++;
-                    p[index].ct = current_time;
-                    p[index].tat = p[index].ct - p[index].at;
-                    p[index].wt = p[index].tat - p[index].bt;
-                    p[index].rt = p[index].start_time - p[index].at;
-                    sum_tat += p[index].tat;
-                    sum_wt += p[index].wt;
-                    sum_rt += p[index].rt;
-                }
-
+            }
+//we have to check if Process on CPU needs to be pushed to Ready Queue
+            if (p[index].bt_remaining > 0)
+                q.add(index);
+// if queue is empty, just add one process from list, whose remaining burst time>0
+            if (q.isEmpty()) {
                 for (int i = 1; i < n; i++) {
-                    if (p[i].bt_remaining > 0 && p[i].at <= current_time && visited[i] == false) {
+                    if (p[i].bt_remaining > 0) {
                         q.add(i);
                         visited[i] = true;
-                    }
-                }
-
-                if (p[index].bt_remaining > 0)
-                    q.add(index);
-
-                if (q.isEmpty()) {
-                    for (int i = 1; i < n; i++) {
-                        if (p[i].bt_remaining > 0) {
-                            q.add(i);
-                            visited[i] = true;
-                            break;
-                        }
+                        break;
                     }
                 }
             }
-            max_completion_time = Integer.MIN_VALUE;
-            for (int i = 0; i < n; i++)
-                max_completion_time = Math.max(max_completion_time, p[i].ct);
-            length_cycle = max_completion_time - p[0].at;
-            //cpu_utilization = (int) ((float) (length_cycle - total_idle_time) / length_cycle * 100);
-
-            Arrays.sort(p, 0, n, new ProcessComparatorPID());
-
-            System.out.println("\nProcess No.\tAT\tCPU Burst Time\tStart Time\tCT\tTAT\tWT\tRT");
-            for (int i = 0; i < n; i++)
-                System.out.println(i + "\t\t" + p[i].at + "\t" + p[i].bt + "\t\t" + p[i].start_time + "\t\t" + p[i].ct + "\t" + p[i].tat + "\t" + p[i].wt + "\t" + p[i].rt);
-            System.out.println();
-
-            System.out.println("\nAverage Turn Around time= " + (float) sum_tat / n);
-            System.out.println("\nAverage Waiting Time= " + (float) sum_wt / n);
-            System.out.println("\nAverage Response Time= " + (float) sum_rt / n);
-            return "" + (float) sum_tat / n;
         }
-    }   
+        // end of the while loop
+        max_completion_time = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++)
+            max_completion_time = Math.max(max_completion_time, p[i].ct);
+        //length_cycle = max_completion_time - p[0].at;
+        //cpu_utilization = (int) ((float) (length_cycle - total_idle_time) / length_cycle * 100);
+
+        //sort so that process ID in output comes in Original order
+        Arrays.sort(p, 0, n, new RobinProcess.ProcessComparatorPID());
+
+// DISPLAYING THE OUTPUT
+        System.out.println("\nProcess No.\tAT\tCPU Burst Time\tStart Time\tCT\tTAT\tWT\tRT");
+        for (int i = 0; i < n; i++)
+            System.out.println(i + "\t\t" + p[i].at + "\t" + p[i].bt + "\t\t" + p[i].start_time + "\t\t" + p[i].ct + "\t" + p[i].tat + "\t" + p[i].wt + "\t" + p[i].rt);
+        System.out.println();
+
+        System.out.println("\nAverage Turn Around time= " + (float) sum_tat / n);
+        System.out.println("\nAverage Waiting Time= " + (float) sum_wt / n);
+        System.out.println("\nAverage Response Time= " + (float) sum_rt / n);
+        return "" + (float) sum_tat / n;
+    }
+}
+
