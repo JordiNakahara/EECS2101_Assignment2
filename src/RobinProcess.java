0126bdd41312d9c8e50ceb0deaf5add4075929bd
Aadi Gautam <124839461+Aadigautam2003@users.noreply.github.com>
2023-11-11 16:29:13 -0500

    Update RobinProcess.java

--- src/RobinProcess.java
+++ src/RobinProcess.java
@@ -1,35 +1,39 @@
-class RobinProcess {
-        int pid;
-        int at;
-        int bt;
-        int ct, wt, tat, rt, start_time;
-        int bt_remaining;
+import java.util.Comparator;
 
-        RobinProcess(int pid, int at, int bt) {
-            this.pid = pid;
-            this.at = at;
-            this.bt = bt;
-            this.ct = 0;
-            this.wt = 0;
-            this.tat = 0;
-            this.rt = 0;
-            this.start_time = 0;
-            this.bt_remaining = bt;
+public class RobinProcess {
+    int pid;
+    int at;
+    int bt;
+    int ct, wt, tat, rt, start_time;
+    int bt_remaining;
+    int p[];
+
+    RobinProcess(int pid, int at, int bt) {
+        this.pid = pid;
+        this.at = at;
+        this.bt = bt;
+        this.ct = 0;
+        this.wt = 0;
+        this.tat = 0;
+        this.rt = 0;
+        this.start_time = 0;
+        this.bt_remaining = bt;
+    }
+
+    static class ProcessComparatorAT implements Comparator<RobinProcess> {
+        public int compare(RobinProcess a, RobinProcess b) {
+            int x = a.at;
+            int y = b.at;
+            return Integer.compare(x, y);
         }
 
-        static class ProcessComparatorAT implements Comparator<RobinProcess> {
-            public int compare(Process a, Process b) {
-                int x = a.at;
-                int y = b.at;
-                return Integer.compare(x, y);
-            }
-        }
+    }
 
-        static class ProcessComparatorPID implements Comparator<RobinProcess> {
-            public int compare(Process a,Process b) {
-                int x = a.pid;
-                int y = b.pid;
-                return Integer.compare(x, y);
-            }
+    static class ProcessComparatorPID implements Comparator<RobinProcess> {
+        public int compare(RobinProcess a,RobinProcess b) {
+            int x = a.pid;
+            int y = b.pid;
+            return Integer.compare(x, y);
         }
+    }
 }
