1566a4decb03ac88c9ff4dbf5bce646dfcaed691
Aadi Gautam <124839461+Aadigautam2003@users.noreply.github.com>
2023-11-11 12:36:51 -0500

    Update Round_Robin.java

--- src/Round_Robin.java
+++ src/Round_Robin.java
@@ -7,38 +7,6 @@ import java.util.LinkedList;
 import java.util.Scanner;
 public class Round_Robin {
 
-static class Process {
-        int pid;
-        int at;
-        int bt;
-        int ct, wt, tat, rt, start_time;
-        int bt_remaining;
-        Process(int pid, int at, int bt) {
-            this.pid = pid;
-            this.at = at;
-            this.bt = bt;
-            this.ct = 0;
-            this.wt = 0;
-            this.tat = 0;
-            this.rt = 0;
-            this.start_time = 0;
-            this.bt_remaining = bt;
-        }
-    }
-    static class ProcessComparatorAT implements Comparator<Process> {
-        public int compare(Process a, Process b) {
-            int x = a.at;
-            int y = b.at;
-            return Integer.compare(x, y);
-        }
-    }
-    static class ProcessComparatorPID implements Comparator<Process> {
-        public int compare(Process a, Process b) {
-            int x = a.pid;
-            int y = b.pid;
-            return Integer.compare(x, y);
-        }
-    }
 
 
     public String scheduler(Process[] ps, int tq){
