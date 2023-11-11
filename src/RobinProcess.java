8dc4686cda8f4161d01a2393ca0bce966a053157
Aadi Gautam <124839461+Aadigautam2003@users.noreply.github.com>
2023-11-11 12:37:50 -0500

    Update RobinProcess.java

--- src/RobinProcess.java
+++ src/RobinProcess.java
@@ -1,8 +1,33 @@
-public class RobinProcess {
-    int arrivalTime;
-    int burstTime;
-    int completionTime;
-    int turnTime;
-    int waitingTime;
-    int startTime;
-}
+public static class Process {
+        int pid;
+        int at;
+        int bt;
+        int ct, wt, tat, rt, start_time;
+        int bt_remaining;
+        Process(int pid, int at, int bt) {
+            this.pid = pid;
+            this.at = at;
+            this.bt = bt;
+            this.ct = 0;
+            this.wt = 0;
+            this.tat = 0;
+            this.rt = 0;
+            this.start_time = 0;
+            this.bt_remaining = bt;
+        }
+    }
+    static class ProcessComparatorAT implements Comparator<Process> {
+        public int compare(Process a, Process b) {
+            int x = a.at;
+            int y = b.at;
+            return Integer.compare(x, y);
+        }
+    }
+    static class ProcessComparatorPID implements Comparator<Process> {
+        public int compare(Process a, Process b) {
+            int x = a.pid;
+            int y = b.pid;
+            return Integer.compare(x, y);
+        }
+    }
+
