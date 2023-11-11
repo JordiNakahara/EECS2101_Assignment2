a08bd7ce4f35614496b6aa561ee50ac4c14e4105
Aadi Gautam <124839461+Aadigautam2003@users.noreply.github.com>
2023-11-11 11:45:20 -0500

    Add files via upload
    Updated code for the round robin scheduling that should resolve the error it was having

--- Round_Robin.java
+++ Round_Robin.java
@@ -1,105 +1,139 @@
+import java.util.*;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.LinkedList;
 import java.util.Scanner;
 public class Round_Robin {
     static class Process {
-        int arrivalTime;
-        int burstTime;
-        int completionTime;
-        int turnTime;
-        int waitingTime;
-        int startTime;
+        int pid;
+        int at;
+        int bt;
+        int ct, wt, tat, rt, start_time;
+        int bt_remaining;
+        Process(int pid, int at, int bt) {
+            this.pid = pid;
+            this.at = at;
+            this.bt = bt;
+            this.ct = 0;
+            this.wt = 0;
+            this.tat = 0;
+            this.rt = 0;
+            this.start_time = 0;
+            this.bt_remaining = bt;
+        }
+    }
+    static class ProcessComparatorAT implements Comparator<Process> {
+        public int compare(Process a, Process b) {
+            int x = a.at;
+            int y = b.at;
+            return Integer.compare(x, y);
+        }
+    }
+    static class ProcessComparatorPID implements Comparator<Process> {
+        public int compare(Process a, Process b) {
+            int x = a.pid;
+            int y = b.pid;
+            return Integer.compare(x, y);
+            //    if(x > y)
+//      return false;  // Apply sorting
+//    return true;   // no sorting
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        Scanner input = new Scanner(System.in);
-        int n, tq;
-        Process[] p = new Process[50];
-        float avgWaiting;
-        int totalTurn = 0;
-        int[] burstArr = new int[50];
-        float avgTurn;
-        int totalWaiting = 0;
-        int index = 0;
-        System.out.print("Enter the number of processes: ");
-        n = input.nextInt();
-        System.out.print("Enter time quantum of CPU: ");
-        tq = input.nextInt();
-        // Use for loop to enter the details of the process
-        //  like Arrival time and the Burst Time
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter arrival time of process " + (i + 1) + ": ");
-            p[i] = new Process();
-            p[i].arrivalTime = input.nextInt();
-            System.out.print("Enter burst time of process " + (i + 1) + ": ");
-            p[i].burstTime = input.nextInt();
-            burstArr[i] = p[i].burstTime;
-            System.out.println();
-        }
-        // We will be using a FIFO queue to implement this algorithm
+        Scanner scanner = new Scanner(System.in);
+        int n, index;
+        //int cpu_utilization;
+        Process[] ps = new Process[50];
         Queue<Integer> q = new LinkedList<>();
-        int current_time = 0;
-        // keep track of the time using a variable-current_time
+        boolean[] visited = new boolean[100];
+        boolean is_first_process = true;
+        int current_time = 0, max_completion_time;
+        int completed = 0, tq, total_idle_time = 0, length_cycle;
+        System.out.print("Enter total number of processes: ");
+        n = scanner.nextInt();
+        float sum_tat = 0, sum_wt = 0, sum_rt = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("\nEnter Process " + i + " Arrival Time: ");
+            int at = scanner.nextInt();
+            ps[i] = new Process(i, at, 0);
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print("\nEnter Process " + i + " Burst Time: ");
+            int bt = scanner.nextInt();
+            ps[i].bt = bt;
+            ps[i].bt_remaining = bt;
+        }
+        System.out.print("\nEnter time quantum: ");
+        tq = scanner.nextInt();
+//sort structure on the basis of Arrival time in increasing order
+        Arrays.sort(ps, 0, n, new ProcessComparatorAT());
         q.add(0);
-        int completed = 0;
-        int[] mark = new int[100];
-        Arrays.fill(mark, 0);
-        mark[0] = 1;
+        visited[0] = true;
         while (completed != n) {
-            //Give quantum unit of time to the process that is in the front of the
-            // queue and pop this process from the queue
-            index = q.peek();
-            q.poll();
-            if (burstArr[index] == p[index].burstTime) {
-                p[index].startTime = Math.max(current_time, p[index].arrivalTime);
-                current_time = p[index].startTime;
+            index = q.poll();
+            if (ps[index].bt_remaining == ps[index].bt) {
+                ps[index].start_time = Math.max(current_time, ps[index].at);
+                total_idle_time += (is_first_process == true) ? 0 : ps[index].start_time - current_time;
+                current_time = ps[index].start_time;
+                is_first_process = false;
             }
-            if (0 < burstArr[index] - tq) {
-                burstArr[index] -= tq;
+            if (ps[index].bt_remaining - tq > 0) {
+                ps[index].bt_remaining -= tq;
                 current_time += tq;
             } else {
-                current_time += burstArr[index];
-                p[index].completionTime = current_time;
-                p[index].turnTime = p[index].completionTime - p[index].arrivalTime;
-                p[index].waitingTime = p[index].turnTime - p[index].burstTime;
-                totalWaiting += p[index].waitingTime;
-                totalTurn += p[index].turnTime;
+                current_time += ps[index].bt_remaining;
+                ps[index].bt_remaining = 0;
                 completed++;
-                burstArr[index] = 0;
+                ps[index].ct = current_time;
+                ps[index].tat = ps[index].ct - ps[index].at;
+                ps[index].wt = ps[index].tat - ps[index].bt;
+                ps[index].rt = ps[index].start_time - ps[index].at;
+                sum_tat += ps[index].tat;
+                sum_wt += ps[index].wt;
+                sum_rt += ps[index].rt;
             }
-            // if some process has arrived when this process was executing,
-            // insert them into the queue
+            //check which new Processes needs to be pushed to Ready Queue from Input list
             for (int i = 1; i < n; i++) {
-                if (burstArr[i] > 0 && p[i].arrivalTime <= current_time && mark[i] == 0) {
-                    mark[i] = 1;
+                if (ps[i].bt_remaining > 0 && ps[i].at <= current_time && visited[i] == false) {
                     q.add(i);
+                    visited[i] = true;
                 }
             }
-            //if the current process has burst time remaining,
-            //push the process into the queue again
-            if (0 < burstArr[index]) {
+//check if Process on CPU needs to be pushed to Ready Queue
+            if (ps[index].bt_remaining > 0)
                 q.add(index);
-            }
-            //if the queue is empty
-            //pick the first process from the list that is not completed
+//if queue is empty, just add one process from list, whose remaining burst time > 0
             if (q.isEmpty()) {
                 for (int i = 1; i < n; i++) {
-                    if (0 < burstArr[i]) {
-                        mark[i] = 1;
+                    if (ps[i].bt_remaining > 0) {
                         q.add(i);
+                        visited[i] = true;
                         break;
                     }
                 }
             }
-            avgWaiting = (float) totalWaiting / n;
-            avgTurn = (float) totalTurn / n;
-            System.out.println();
-            // to get the data from each process we
-            System.out.println("Average Waiting Time = " + avgWaiting);
-            System.out.println("Average Turnaround Time = " + avgTurn);
         }
+//end of while
+        //Calculate Length of Process completion cycle
+        max_completion_time = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++)
+            max_completion_time = Math.max(max_completion_time, ps[i].ct);
+        length_cycle = max_completion_time - ps[0].at;
+        //ps[0].start_time;
+
+        Arrays.sort(ps, 0, n, new ProcessComparatorPID());
+        //Output
+        System.out.println("\nProcess No.\tAT\tCPU Burst Time\tStart Time\tCT\tTAT\tWT\tRT");
+        for (int i = 0; i < n; i++)
+            System.out.println(i + "\t\t\t\t" + ps[i].at + "\t\t\t" + ps[i].bt + "\t\t\t\t" + ps[i].start_time + "\t\t\t\t" + ps[i].ct + "\t\t\t" + ps[i].tat + "\t\t\t" + ps[i].wt + "\t\t\t\t" + ps[i].rt);
+        System.out.println();
+        System.out.println("\nAverage Turn Around time= " + (float) sum_tat / n);
+        System.out.println("\nAverage Waiting Time= " + (float) sum_wt / n);
+
     }
 }
