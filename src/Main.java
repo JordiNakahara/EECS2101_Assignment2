3e5e54f1aa91c8e8c95e0d7c344f4fd6a4341181
Jordi Nakahara <jordi13@my.yorku.ca>
2023-11-13 20:53:18 -0500

    Create Main.java
    Added a Main class with a main method. Currently only valid for SJF.

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean isValidInput = false;
+        Scanner sc = new Scanner(System.in);
+        String algorithm = "None chosen";
+        while (!isValidInput) {
+            System.out.println("Please input either \"round robin\", \"shortest job first\", or \"round robin priority\":");
+            algorithm = sc.nextLine().toLowerCase();
+
+            if (algorithm.equals("round robin")) {
+                isValidInput = true;
+
+            }
+            else if (algorithm.equals("shortest job first")) {
+                isValidInput = true;
+            }
+            else if (algorithm.equals("round robin priority")) {
+                isValidInput = true;
+            }
+            else {
+                System.out.println("Please enter valid input\n-------------------------\n");
+            }
+        }
+
+        System.out.println("Algorithm selected: " + algorithm);
+        isValidInput = false;
+
+        while (!isValidInput) {
+            if (algorithm.equals("round robin")) {
+
+            } else if (algorithm.equals("shortest job first")) {
+                try {
+
+                    System.out.println("Please enter the number of processes:");
+                    int numProcess = Integer.parseInt(sc.nextLine());
+                    int[] arrivalTimes = new int[numProcess];
+                    int[] burstTimes = new int[numProcess];
+                    ArrayList<SJFProcess> toPass = new ArrayList<SJFProcess>();
+
+                    for (int i = 0; i < numProcess; i++) {
+                        System.out.println("Please enter arrival time number " + (i + 1));
+                        arrivalTimes[i] = Integer.parseInt(sc.nextLine());
+                    }
+
+                    for (int i = 0; i < numProcess; i++) {
+                        System.out.println("Please enter burst time number " + (i + 1));
+                        burstTimes[i] = Integer.parseInt(sc.nextLine());
+                    }
+
+                    for (int i = 0; i < numProcess; i++) {
+                        toPass.add(new SJFProcess("SJFProcess" + i,
+                                burstTimes[i], arrivalTimes[i]));
+                    }
+
+                    SJFQueue toRun = new SJFQueue(toPass);
+                    toRun.averageWaitingTime();
+                    String output = "" + toRun.averageWaiting();
+                    System.out.println("**********************************************\nSJF waiting time = " + output);
+                    isValidInput = true;
+                } catch (Exception e) {
+                    System.out.println("Please enter valid input!");
+                    isValidInput = false;
+                }
+            }
+        }
+
+
+
+    }
+}
